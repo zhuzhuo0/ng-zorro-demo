d76e6955f98014604472df5ab9ca86bb973b4cfb
DESKTOP-N0FMIGE\猪猪 <422914219@qq.com>
2018-06-06 22:35:15 +0800

    初始化项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
-
+/* 注册语言包 */
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
